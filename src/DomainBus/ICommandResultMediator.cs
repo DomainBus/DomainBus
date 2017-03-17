e418f51a0c30ca828be7e5b2af6eb1194f0c2e6e
Mihai Mogosanu <mike@sapiensworks.com>
2017-03-16 20:51:28 -0400

    added Command result mediator from CavemanTools

---
+++ src/DomainBus/ICommandResultMediator.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace CavemanTools.Infrastructure
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ICommandResultMediator
+    {
+        void AddResult<T>(Guid cmdId, T result) where T :class;
+        /// <summary>
+        /// Used to await a command result
+        /// </summary>
+        /// <param name="cmdId"></param>
+        /// <param name="timeout">Default is 5s</param>
+        /// <returns></returns>
+        IResultListener GetListener(Guid cmdId,TimeSpan? timeout=null);
+    }
+}
