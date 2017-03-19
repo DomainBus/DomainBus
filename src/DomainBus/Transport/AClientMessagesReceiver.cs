6b8821acfbcbe8aa507856b476c0b5d807df181a
Mihai Mogosanu <mike@sapiensworks.com>
2017-03-19 19:00:10 -0400

    changed implementation to use ITimer

--- src/DomainBus/Transport/AClientMessagesReceiver.cs
+++ src/DomainBus/Transport/AClientMessagesReceiver.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 using System.Linq;
+using CavemanTools;
 using CavemanTools.Logging;
 using DomainBus.Dispatcher.Server;
 
 namespace DomainBus.Transport
 {
-    public abstract class AClientMessagesReceiver : AbstractPoller, IGetMessages
+    public abstract class AClientMessagesReceiver : IDisposable, IGetMessages
     {
+        protected readonly ITimer Timer;
         private IRouteMessages _server;
+
+        protected AClientMessagesReceiver():this(new DefaultTimer())
+        {
+            
+        }
+
+        protected AClientMessagesReceiver(ITimer timer)
+        {
+            Timer = timer;
+            Timer.Interval = 30.ToSeconds();
+            Timer.SetHandler(Callback);
+        }
 
         public void Subscribe(IRouteMessages router)
         {
             _server = router;
         }
 
-        protected override void Callback(object state)
+        protected void Callback(object state)
         {
             var items = GetMessages();
 
@@ -44,5 +58,14 @@ namespace DomainBus.Transport
 
         protected abstract EnvelopeFromClient[] GetMessages();
         protected abstract void MarkAsHandled(EnvelopeFromClient envs);
+        public void Dispose()
+        {
+            Timer.Dispose();
+        }
+
+        public void Start()
+        {
+           Timer.Start();
+        }
     }
 }
--- src/DomainBus/Transport/AEndpointConfigurationReceiver.cs
+++ src/DomainBus/Transport/AEndpointConfigurationReceiver.cs
@@ -1,21 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using CavemanTools;
 using CavemanTools.Logging;
 using DomainBus.Dispatcher.Client;
 using DomainBus.Dispatcher.Server;
 
 namespace DomainBus.Transport
 {
-    public abstract class AEndpointConfigurationReceiver : AbstractPoller, IGetEndpointUpdates
+    public abstract class AEndpointConfigurationReceiver : IDisposable, IGetEndpointUpdates
     {
+        protected readonly ITimer Timer;
         private IWantEndpointUpdates _server;
+
+        protected AEndpointConfigurationReceiver():this(new DefaultTimer())
+        {
+            
+        }
+
+        protected AEndpointConfigurationReceiver(ITimer timer)
+        {
+            timer.MustNotBeNull();
+            Timer = timer;
+            Timer.Interval = 30.ToSeconds();
+            Timer.SetHandler(Callback);
+        }
+
+        public void Dispose()
+        {
+            Timer.Dispose();
+        }
+
+        public void Start()
+        {
+            Timer.Start();
+        }
 
         public void Subscribe(IWantEndpointUpdates server)
         {
             _server = server;          
         }
 
-        protected override void Callback(object state)
+        protected void Callback(object state)
         {
             try
             {
